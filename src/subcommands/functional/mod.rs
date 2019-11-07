213d6c3c795f0ed3aad884a98f7b448aafc24795
keroro520 <keroroxx520@gmail.com>
2019-11-07 14:10:44 +0800

    feat: Add spendable utils

--- src/subcommands/functional/mod.rs
+++ src/subcommands/functional/mod.rs
@@ -1,9 +1,14 @@
 mod chain_client;
 mod index_client;
 mod secp_signer;
+mod spendable;
 
 pub use chain_client::ChainClient;
 pub use index_client::IndexClient;
 pub use secp_signer::{
     blake2b_args, build_secp_witnesses, serialize_signature, sign_with_keystore, sign_with_privkey,
+};
+pub use spendable::{
+    can_deposit, can_prepare, can_spend, can_withdraw, is_live_cell, is_live_deposit_cell,
+    is_live_prepare_cell, is_live_secp_cell, is_none_type_cell,
 };
